809251d0399584f14a8842001c8786f34babe1e9
ocd0 <88198927+ocd0@users.noreply.github.com>
2022-02-18 15:17:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Olus scientific research pipelines'));
+app.get('/', (req, res) => res.send('Olus scientific research pipelines v2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
